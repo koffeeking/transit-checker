cf6c3fe3a8d3c28f89c03e23ea2c8128e958b401
chandler <chandler@hioscar.com>
2023-03-05 12:02:44 +0000

    get transit checker working

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
-'''print the next g train'''
+"""print the next g train"""
 from utils import get_next_g_train
 
-get_next_g_train(direction='Church')
-
-# from google.transit import gtfs_realtime_pb2
-# import urllib3
-
-# http = urllib3.PoolManager()
-# feed = gtfs_realtime_pb2.FeedMessage()
-# response = http.request('GET', r"https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g")
-# feed.ParseFromString(response.read())
-
-# print(feed.entity)
-# for entity in feed.entity:
-#     if entity.HasField('trip_update'):
-#         print(entity.trip_update)
+if __name__ == "__main__":
+    get_next_g_train()
--- utils.py
+++ utils.py
@@ -1,45 +1,42 @@
-'''utils for printing the next g train time'''
+"""utils for printing the next g train time"""
 from datetime import datetime
 import re
 import requests
 from google.transit import gtfs_realtime_pb2
+from pytz import timezone
 
 
-def get_next_g_train(direction: str):
-    # determine direction ID based on input
-    direction_id = 0
-    if re.fullmatch(pattern=r'[Cc][Hh][Uu][Rr][Cc][Hh]', string=direction):
-        direction_id = "0"
-    elif re.fullmatch(pattern=r'[Qq][Uu][Ee][Ee][Nn][Ss]', string=direction):
-        direction_id = "1"
+def get_next_g_train():
+    """returns a list of the next G trains departing from Bedford-Nostrand"""
 
     # Set up the API endpoint and parameters
     url = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-g"
-    params = {
-        "key": "Gb8N3T90sX2Nc6h3DBZhT8ixHwlUAjpT59tS9HO3",
-        "feed_id": "31",
-        "station_id": "G26S",
-        "direction_id": direction_id,
-        "time": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    }
     headers = {
         "x-api-key": "Gb8N3T90sX2Nc6h3DBZhT8ixHwlUAjpT59tS9HO3",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     # Make the API request and parse the responsed
-    response = requests.get(url, params=params, headers=headers)
-    feed = gtfs_realtime_pb2.TripUpdate.StopTimeUpdate()
+    response = requests.get(
+        url,
+        headers=headers,
+    )
+    feed = gtfs_realtime_pb2.FeedMessage()
     feed.ParseFromString(response.content)
 
-    # with open('test_file.json','w+') as test_file:
-    #     test_file.write(str(feed.entity))
-
-    print(feed)
-
-    #         print(entity.trip_update)
-
-    # next_arrival = data["data"][0]["attributes"]["arrival_time"]
-
-    # # Return the time of the next G train's arrival
-    # return datetime.strptime(next_arrival, "%Y-%m-%dT%H:%M:%S%z").strftime("%I:%M %p")
+    print("------Queens------")
+    for entity in feed.entity:
+        for update in entity.trip_update.stop_time_update:
+            if update.stop_id == "G26N":
+                utc_time = datetime.fromtimestamp(update.arrival.time)
+                ny_time = utc_time.astimezone(timezone("US/Eastern"))
+                time_til = int((utc_time - datetime.utcnow()).total_seconds() / 60)
+                print(f"arrival: {ny_time} (in {time_til} minutes)")
+    print("------Church------")
+    for entity in feed.entity:
+        for update in entity.trip_update.stop_time_update:
+            if update.stop_id == "G26S":
+                utc_time = datetime.fromtimestamp(update.arrival.time)
+                ny_time = utc_time.astimezone(timezone("US/Eastern"))
+                time_til = int((utc_time - datetime.utcnow()).total_seconds() / 60)
+                print(f"arrival: {ny_time} (in {time_til} minutes)")
